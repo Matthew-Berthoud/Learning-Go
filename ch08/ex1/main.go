cdd8cef8755ce13b37c0d5b49812e7918256c4b0
Matthew Berthoud <mwberthoud@wm.edu>
2025-02-12 12:21:58 -0500

    including all numeric values

--- ch08/ex1/main.go
+++ ch08/ex1/main.go
@@ -6,7 +6,7 @@ import "fmt"
 // Define any needed generic interfaces.
 
 type Numeric interface {
-	int | float64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
 }
 
 func doubleNumber[T Numeric](number T) T {
@@ -17,6 +17,7 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(doubleNumber(i))
 	}
+	fmt.Println(doubleNumber(14.8))
 	// fmt.Println(doubleNumber(true))
 	// fmt.Println(doubleNumber("not Numeric"))
 }
