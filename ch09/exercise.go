dd9ae5088eadddcdde53c0e5fbda52ab06317886
Matthew Berthoud <mwberthoud@wm.edu>
2025-02-20 15:09:18 -0500

    ex1 setup

--- ch09/exercise.go
+++ ch09/exercise.go
@@ -7,6 +7,9 @@ import (
 	"regexp"
 	"strings"
 )
+// Create a sentinel error to represent an invalid ID.
+// In main, use errors.Is to check for the sentinel error,
+// and print a message when it is found.
 
 func main() {
 	d := json.NewDecoder(strings.NewReader(data))
