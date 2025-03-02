ce0a0816d29995ea140db880b2dd32698dbc5487
Matthew Berthoud <mwberthoud@wm.edu>
2025-03-02 12:44:32 -0500

    workspace example

---
+++ ch10/my_workspace/workspace_app/app.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "github.com/Matthew-Berthoud/workspace_lib"
+)
+
+func main() {
+    fmt.Println(workspace_lib.AddNums(2, 3))
+    fmt.Println(workspace_lib.SubNums(2, 3))
+}
+
---
+++ ch10/my_workspace/workspace_app/go.mod
@@ -0,0 +1,5 @@
+module github.com/Matthew-Berthoud/workspace_app
+
+go 1.18
+
+require github.com/Matthew-Berthoud/workspace_lib v0.0.0-20250302173716-e47695f69219
