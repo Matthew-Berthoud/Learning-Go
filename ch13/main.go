5e6decb1fecffbfd98061c445ac64e205f47eae8
Matthew Berthoud <matthewberthoud@gmail.com>
2025-03-29 13:57:15 -0400

    ex1

---
+++ ch13/main.go
@@ -0,0 +1,53 @@
+/*
+Exercises
+
+1.	Write a small web server that returns the current time in RFC 3339 format
+	when you send it a GET command. You can use a third-party module if you’d
+	like.
+
+2.	Write a small middleware component that uses JSON structured logging to
+	log the IP address of each incoming request to your web server.
+
+3.	Add the ability to return the time as JSON. Use the Accept header to
+	control whether JSON or text is returned (default to text). The JSON
+	should be structured as follows:
+{
+    "day_of_week": "Monday",
+    "day_of_month": 10,
+    "month": "April",
+    "year": 2023,
+    "hour": 20,
+    "minute": 15,
+    "second": 20
+}
+*/
+
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+type TimeHandler struct{}
+
+func (hh TimeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().Format(time.RFC3339)
+	w.Write([]byte(t + "\n"))
+}
+
+func main() {
+	s := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      TimeHandler{},
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		if err != http.ErrServerClosed {
+			panic(err)
+		}
+	}
+}
